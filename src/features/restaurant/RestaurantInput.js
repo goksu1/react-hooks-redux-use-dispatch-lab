47aa7f9b012cf47a6dffee5195dceb14dbe7bc7f
goksu1 <goksu.baykal@gmail.com>
2022-03-10 01:42:57 +0300

    Done.

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { addRestaurant } from "./restaurantsSlice";
+import {useDispatch} from 'react-redux'
 
 function RestaurantInput() {
+  const dispatch = useDispatch()
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
 
@@ -15,7 +17,7 @@ function RestaurantInput() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // add missing code
+    dispatch(addRestaurant({name,location}))
   }
 
   return (
